6b79f026784dd4a8a9820d2eb64b9b738b1773bc
Alana Gilston <abluescarab@users.noreply.github.com>
2023-05-06 00:48:29 -0700

    Removed commented code; implemented offer delete

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -421,231 +421,51 @@ async function AppRoutes(app: FastifyInstance, _options = {}) {
   });
 
   // DELETE - mark an offer as deleted
-  app.delete("/offers", async (request, reply) => {});
+  app.delete<{ Body: { buyer_email: string; listing_name: string } }>(
+    "/offers",
+    async (request, reply) => {
+      const { buyer_email, listing_name } = request.body;
+
+      try {
+        const buyer = await request.em.findOne(User, { email: buyer_email });
+
+        if (buyer === null || buyer.deleted_at !== null) {
+          return error(reply, 404, `User with email ${buyer_email} not found`);
+        }
+
+        const listing = await request.em.findOne(Listing, {
+          name: listing_name,
+        });
+
+        if (listing === null || listing.deleted_at !== null) {
+          return error(
+            reply,
+            404,
+            `Listing with name ${listing_name} not found`
+          );
+        }
+
+        const offer = await request.em.findOne(Offer, { buyer, listing });
+
+        if (offer === null || offer.deleted_at !== null) {
+          return error(
+            reply,
+            404,
+            `User with email ${buyer_email} has not made an offer on listing with name ${listing_name}`
+          );
+        }
+
+        offer.deleted_at = new Date();
+        await request.em.flush();
+
+        console.log(offer);
+        return reply.send(offer);
+      } catch (err) {
+        return error(reply, 404, err.message);
+      }
+    }
+  );
   // endregion
-
-  // //----------------------------------------------------------------------------
-  // // Match Functionality
-  // // add match
-  // app.post<{
-  //   Body: { email: string; matchee_email: string };
-  // }>("/match", async (request, reply) => {
-  //   const { email, matchee_email } = request.body;
-  //
-  //   try {
-  //     const matchee = await request.em.findOne(User, {
-  //       email: matchee_email,
-  //     });
-  //     const owner = await request.em.findOne(User, { email });
-  //     const match = await request.em.create(Match, { owner, matchee });
-  //
-  //     if (matchee === null || matchee.deleted_at !== null) {
-  //       return error(
-  //         reply,
-  //         404,
-  //         `User with email address ${matchee_email} not found`
-  //       );
-  //     }
-  //
-  //     if (owner === null || owner.deleted_at !== null) {
-  //       return error(reply, 404, `User with email address ${email} not found`);
-  //     }
-  //
-  //     await request.em.flush();
-  //     console.log(match);
-  //     return reply.send(match);
-  //   } catch (err) {
-  //     return error(reply, 500, err.message);
-  //   }
-  // });
-
-  // //----------------------------------------------------------------------------
-  // // Message Functionality
-  // // read all received messages
-  // app.search<{
-  //   Body: { receiver: string };
-  // }>("/messages", async (request, reply) => {
-  //   const { receiver } = request.body;
-  //
-  //   try {
-  //     const theReceiver = await request.em.findOne(User, { email: receiver });
-  //
-  //     if (theReceiver === null || theReceiver.deleted_at !== null) {
-  //       return error(
-  //         reply,
-  //         404,
-  //         `User with email address ${receiver} not found`
-  //       );
-  //     }
-  //
-  //     const messages = await request.em.find(Message, {
-  //       receiver: theReceiver,
-  //       deleted_at: null,
-  //     });
-  //     console.log(messages);
-  //     reply.send(messages);
-  //   } catch (err) {
-  //     return error(reply, 500, err.message);
-  //   }
-  // });
-  //
-  // // read all sent messages
-  // app.search<{
-  //   Body: { sender: string };
-  // }>("/messages/sent", async (request, reply) => {
-  //   const { sender } = request.body;
-  //
-  //   try {
-  //     const theSender = await request.em.findOne(User, { email: sender });
-  //
-  //     if (theSender === null || theSender.deleted_at !== null) {
-  //       return error(reply, 404, `User with email address ${sender} not found`);
-  //     }
-  //
-  //     const messages = await request.em.find(Message, {
-  //       sender: theSender,
-  //       deleted_at: null,
-  //     });
-  //     console.log(messages);
-  //     reply.send(messages);
-  //   } catch (err) {
-  //     console.error(err);
-  //     reply.status(500).send(err);
-  //   }
-  // });
-  //
-  // // create message
-  // app.post<{
-  //   Body: { sender: string; receiver: string; message: string };
-  // }>("/messages", async (request, reply) => {
-  //   const { sender, receiver, message } = request.body;
-  //
-  //   try {
-  //     if (hasBadWord(message)) {
-  //       const err =
-  //         "Failed to create new message: Inappropriate language in message";
-  //       console.log(err);
-  //       return reply.status(500).send({ message: err });
-  //     }
-  //
-  //     const theSender = await request.em.findOne(User, { email: sender });
-  //     const theReceiver = await request.em.findOne(User, { email: receiver });
-  //
-  //     if (theSender === null || theSender.deleted_at !== null) {
-  //       return error(reply, 404, `User with email address ${sender} not found`);
-  //     }
-  //
-  //     if (theReceiver === null || theReceiver.deleted_at !== null) {
-  //       return error(
-  //         reply,
-  //         404,
-  //         `User with email address ${receiver} not found`
-  //       );
-  //     }
-  //
-  //     const newMessage = await request.em.create(Message, {
-  //       sender: theSender,
-  //       receiver: theReceiver,
-  //       message,
-  //     });
-  //
-  //     await request.em.flush();
-  //     console.log(newMessage);
-  //     return reply.send(newMessage);
-  //   } catch (err) {
-  //     return error(reply, 500, `Failed to create new message: ${err.message}`);
-  //   }
-  // });
-  //
-  // // update message
-  // app.put<{
-  //   Body: { messageId: number; message: string };
-  // }>("/messages", async (request, reply) => {
-  //   const { messageId, message } = request.body;
-  //
-  //   const theMessage = await request.em.findOne(Message, {
-  //     messageId: messageId,
-  //   });
-  //
-  //   if (theMessage === null || theMessage.deleted_at !== null) {
-  //     return error(reply, 404, `Message with ID ${messageId} not found`);
-  //   }
-  //
-  //   theMessage.message = message;
-  //
-  //   // persist object changes to database
-  //   await request.em.flush();
-  //   console.log(theMessage);
-  //   reply.send(theMessage);
-  // });
-  //
-  // // delete message
-  // app.delete<{
-  //   Body: { messageId: number; password: string };
-  // }>("/messages", async (request, reply) => {
-  //   const { messageId, password } = request.body;
-  //
-  //   try {
-  //     if (password !== process.env.ADMIN_PASS) {
-  //       const err = "Failed to delete message: Invalid admin password";
-  //       console.error(err);
-  //       return reply.status(401).send({ message: err });
-  //     }
-  //
-  //     const message = await request.em.findOne(Message, { messageId });
-  //
-  //     if (message === null || message.deleted_at !== null) {
-  //       return error(reply, 404, `Message with ID ${messageId} not found`);
-  //     }
-  //
-  //     message.deleted_at = new Date();
-  //
-  //     await request.em.flush();
-  //
-  //     // await request.em.remove(message).flush();
-  //     console.log(message);
-  //     reply.send(message);
-  //   } catch (err) {
-  //     return error(reply, 500, err.message);
-  //   }
-  // });
-  //
-  // // delete all messages
-  // app.delete<{
-  //   Body: { sender: string; password: string };
-  // }>("/messages/all", async (request, reply) => {
-  //   const { sender, password } = request.body;
-  //
-  //   try {
-  //     if (password !== process.env.ADMIN_PASS) {
-  //       const err = "Failed to delete messages: Invalid admin password";
-  //       console.error(err);
-  //       return reply.status(401).send({ message: err });
-  //     }
-  //
-  //     const theSender = await request.em.findOne(User, { email: sender });
-  //
-  //     if (theSender === null || theSender.deleted_at !== null) {
-  //       return error(reply, 404, `User with email address ${sender} not found`);
-  //     }
-  //
-  //     const messages = await request.em.find(Message, {
-  //       sender: theSender,
-  //       deleted_at: null,
-  //     });
-  //
-  //     messages.forEach((m) => {
-  //       m.deleted_at = new Date();
-  //     });
-  //
-  //     // await request.em.remove(messages).flush();
-  //     await request.em.flush();
-  //     console.log(messages);
-  //     reply.send(messages);
-  //   } catch (err) {
-  //     return error(reply, 500, err.message);
-  //   }
-  // });
 }
 
 export default AppRoutes;
