87dff8d1ad943ba9026de845964b8b8a7fd70576
Alana Gilston <abluescarab@users.noreply.github.com>
2023-05-07 02:14:26 -0700

    Added SASS locally and changed to .scss files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "@images/react.svg";
 import viteLogo from "/vite.svg";
-import "@css/App.css";
+import "@css/App.scss";
 
 function App() {
   const [count, setCount] = useState(0);
