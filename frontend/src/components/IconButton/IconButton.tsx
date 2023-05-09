58d425f53b8ab480cd0f457f400fc8f9167234dd
Alana Gilston <abluescarab@users.noreply.github.com>
2023-05-09 01:39:07 -0700

    Added onClick handler to IconButton

--- frontend/src/components/IconButton/IconButton.tsx
+++ frontend/src/components/IconButton/IconButton.tsx
@@ -1,21 +1,24 @@
 import "./IconButton.scss";
 import Button from "@/components/Button/Button.tsx";
-import React from "react";
+import React, { MouseEventHandler } from "react";
 
 interface IconButtonProps {
   color?: "primary" | "secondary";
   className?: string;
   icon: string;
+  onClick?: MouseEventHandler;
 }
 
 const IconButton: React.FC<IconButtonProps> = ({
   color = "",
   className = "",
   icon,
+  onClick = null,
 }) => {
   return (
     <Button
       className={`icon-button material-symbols-rounded ${color} ${className}`}
+      onClick={onClick}
     >
       {icon}
     </Button>
