fb4eacd58f3bad4fdfe69ddc3ffa1688c7c4c86f
Alana Gilston <abluescarab@users.noreply.github.com>
2023-05-08 23:36:01 -0700

    Created style for circular buttons

--- frontend/src/components/Carousel/Carousel.tsx
+++ frontend/src/components/Carousel/Carousel.tsx
@@ -9,10 +9,10 @@ const Carousel: React.FC<CarouselProps> = ({ children = null }) => {
   return (
     <div className={"carousel-container"}>
       <div className={"carousel-controls"}>
-        <button className={"material-symbols-rounded left-button"}>
+        <button className={"material-symbols-rounded left-button icon"}>
           chevron_left
         </button>
-        <button className={"material-symbols-rounded right-button"}>
+        <button className={"material-symbols-rounded right-button icon"}>
           chevron_right
         </button>
       </div>
