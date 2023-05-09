deffcc6b8412ecdfc79e031ab1971fc8d3028478
Alana Gilston <abluescarab@users.noreply.github.com>
2023-05-09 01:39:16 -0700

    Implemented carousel left/right

--- frontend/src/components/Carousel/Carousel.tsx
+++ frontend/src/components/Carousel/Carousel.tsx
@@ -1,5 +1,5 @@
 import "./Carousel.scss";
-import React from "react";
+import React, { Children, useEffect, useState } from "react";
 import IconButton from "@/components/IconButton/IconButton.tsx";
 
 interface CarouselProps {
@@ -7,13 +7,55 @@ interface CarouselProps {
 }
 
 const Carousel: React.FC<CarouselProps> = ({ children = null }) => {
+  const [left, setLeft] = useState(0);
+  const [buttonsVisible, setButtonsVisible] = useState(true);
+
+  const style = {
+    left: `calc((var(--listing-width) + var(--listing-margin) * 2) * 3 * ${left})`,
+  };
+
+  useEffect(() => {
+    console.log("use");
+    if (Children.count(children) > 3) {
+      setButtonsVisible(true);
+    } else {
+      setButtonsVisible(false);
+    }
+  }, [children]);
+
+  const moveLeft = () => {
+    if (left < 0) {
+      setLeft(left + 1);
+    }
+  };
+
+  const moveRight = () => {
+    const pageCount = Math.ceil(Children.count(children) / 3);
+
+    if (left > -pageCount + 1) {
+      setLeft(left - 1);
+    }
+  };
+
   return (
     <div className={"carousel-container"}>
-      <div className={"carousel-controls"}>
-        <IconButton className={"left-button"} icon={"chevron_left"} />
-        <IconButton className={"right-button"} icon={"chevron_right"} />
+      {buttonsVisible ? (
+        <div className={"carousel-controls"}>
+          <IconButton
+            className={"left-button"}
+            icon={"chevron_left"}
+            onClick={() => moveLeft()}
+          />
+          <IconButton
+            className={"right-button"}
+            icon={"chevron_right"}
+            onClick={() => moveRight()}
+          />
+        </div>
+      ) : null}
+      <div className={"carousel"} style={style}>
+        {children}
       </div>
-      <div className={"carousel"}>{children}</div>
     </div>
   );
 };
