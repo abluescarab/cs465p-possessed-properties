a735eaf3fc38aaf178bdabcbc1b00c20c3a81e09
Alana Gilston <abluescarab@users.noreply.github.com>
2023-05-12 04:16:49 -0700

    Implemented search bar with dropdown

--- frontend/src/components/SearchBar/SearchBar.tsx
+++ frontend/src/components/SearchBar/SearchBar.tsx
@@ -3,23 +3,75 @@ import Card from "@/components/Card/Card.tsx";
 import CardContent from "@/components/Card/CardContent.tsx";
 import TextInput from "@/components/TextInput/TextInput.tsx";
 import Button from "@/components/Button/Button.tsx";
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const SearchBar = () => {
+  const [listings, setListings] = useState([]);
+  const resultsDiv = useRef(null);
+
+  async function fetchListings(searchTerm: string) {
+    const { data } = await axios.get("http://localhost:8080/listings");
+    const listingItems = [];
+
+    if (searchTerm.length > 0) {
+      data.forEach((listing) => {
+        if (listing.name.toLowerCase().startsWith(searchTerm)) {
+          console.log(`pushing ${listing.name}`);
+          listingItems.push({ id: listing.id, name: listing.name });
+        }
+      });
+
+      listingItems.sort((a, b) => a.name.localeCompare(b.name));
+    }
+
+    return listingItems;
+  }
+
+  useEffect(() => {
+    if (resultsDiv.current) {
+      if (listings.length > 0) {
+        resultsDiv.current.classList.add("open");
+      } else {
+        resultsDiv.current.classList.remove("open");
+      }
+    }
+  }, [listings]);
+
+  async function search(e) {
+    await fetchListings(e.currentTarget.value).then(setListings);
+  }
+
   return (
-    <Card className={"search"} shadow={"hover"}>
-      <CardContent>
-        <form>
-          <TextInput
-            name={"search"}
-            placeholder={"Search for listings..."}
-            className={"font-lg"}
-          />
-          <Button type={"submit"}>
-            <span className={"material-symbols-rounded dark"}>search</span>
-          </Button>
-        </form>
-      </CardContent>
-    </Card>
+    <div className={"search"}>
+      <Card shadow={"hover"}>
+        <CardContent>
+          <form>
+            <TextInput
+              name={"search"}
+              placeholder={"Search for listings..."}
+              className={"font-lg"}
+              onChange={search}
+            />
+            <Button type={"submit"}>
+              <span className={"material-symbols-rounded dark"}>search</span>
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+      <div className={"search-results"} ref={resultsDiv}>
+        <ul id={"listings-view"}>
+          {listings.map((listing) => {
+            return (
+              <li key={listing}>
+                <Link to={`/listings/${listing.id}`}>{listing.name}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
   );
 };
 
