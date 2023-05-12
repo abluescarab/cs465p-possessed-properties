635e9c63d2f902429bc4331e4d9c57c987ecacd7
Alana Gilston <abluescarab@users.noreply.github.com>
2023-05-12 01:37:20 -0700

    Designed forgot password and sign up pages

--- frontend/src/pages/ForgotPassword/ForgotPassword.tsx
+++ frontend/src/pages/ForgotPassword/ForgotPassword.tsx
@@ -1,8 +1,50 @@
 import "./ForgotPassword.scss";
+import Card from "@/components/Card/Card.tsx";
+import CardContent from "@/components/Card/CardContent.tsx";
+import TextInput from "@/components/TextInput/TextInput.tsx";
+import Button from "@/components/Button/Button.tsx";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { setTitle } from "@/utils.tsx";
 
 const ForgotPassword = () => {
+  useEffect(() => {
+    setTitle("Forgot Password");
+  });
+
   return (
-    <></>
+    <section>
+      <Card className={"auth"}>
+        <CardContent>
+          <p className={"font-sm"}>
+            Enter the email address associated with your account and we'll send
+            you instructions to reset your password.
+          </p>
+          <form>
+            <div className={"input-line"}>
+              <TextInput
+                id={"email"}
+                label={"Email"}
+                name={"email"}
+                placeholder={"e.g. yourname@email.com"}
+                type={"email"}
+                className={"underline"}
+              />
+            </div>
+            <div className={"input-line"}>
+              <Button type={"submit"} color={"primary"}>
+                Submit
+              </Button>
+            </div>
+            <div className={"input-line"}>
+              <Link to={"/signin"} className={"font-sm"}>
+                Back to sign in
+              </Link>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </section>
   );
 };
 
--- frontend/src/pages/SignUp/SignUp.tsx
+++ frontend/src/pages/SignUp/SignUp.tsx
@@ -1,13 +1,67 @@
 import "./SignUp.scss";
 import { useEffect } from "react";
 import { setTitle } from "@/utils.tsx";
+import Card from "@/components/Card/Card.tsx";
+import CardContent from "@/components/Card/CardContent.tsx";
+import TextInput from "@/components/TextInput/TextInput.tsx";
+import Button from "@/components/Button/Button.tsx";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   useEffect(() => {
     setTitle("Sign Up");
   });
 
-  return <div></div>;
+  return (
+    <>
+      <section>
+        <Card className={"auth"}>
+          <CardContent>
+            <form>
+              <div className={"input-line"}>
+                <TextInput
+                  id={"email"}
+                  label={"Email"}
+                  name={"email"}
+                  placeholder={"e.g. yourname@email.com"}
+                  type={"email"}
+                  className={"underline"}
+                />
+              </div>
+              <div className={"input-line"}>
+                <TextInput
+                  id={"password"}
+                  label={"Password"}
+                  name={"password"}
+                  placeholder={"e.g. hunter2"}
+                  className={"underline"}
+                />
+              </div>
+              <div className={"input-line"}>
+                <TextInput
+                  id={"confirm-password"}
+                  label={"Confirm password"}
+                  name={"confirm-password"}
+                  placeholder={"Passwords must match"}
+                  className={"underline"}
+                />
+              </div>
+              <div className={"input-line"}>
+                <Button type={"submit"} color={"primary"}>
+                  Sign up
+                </Button>
+              </div>
+            </form>
+          </CardContent>
+        </Card>
+      </section>
+      <section className={"auth-links"}>
+        <p className={"center-text font-sm"}>
+          Already have an account?&nbsp;<Link to={"/signin"}>Sign in</Link>
+        </p>
+      </section>
+    </>
+  );
 };
 
 export default SignUp;
