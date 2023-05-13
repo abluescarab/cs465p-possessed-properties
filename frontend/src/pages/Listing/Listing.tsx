9cf7e0f3c3691aeb4bea4ceef6679c102b7e7891
Alana Gilston <abluescarab@users.noreply.github.com>
2023-05-12 19:26:56 -0700

    Added listing info to listing page

--- frontend/src/pages/Listing/Listing.tsx
+++ frontend/src/pages/Listing/Listing.tsx
@@ -3,6 +3,7 @@ import { useLoaderData } from "react-router-dom";
 import { useEffect } from "react";
 import { setTitle } from "@/utils.tsx";
 import propertyImage from "@images/property.png";
+import Button from "@/components/Button/Button.tsx";
 
 const Listing = () => {
   const listing: any = useLoaderData();
@@ -17,7 +18,21 @@ const Listing = () => {
         <img src={propertyImage} alt={`Image of ${listing.name}`} />
       </div>
       <h2 className={"listing-name"}>{listing.name}</h2>
-      <div className={"listing-info"}>&nbsp;</div>
+      <div className={"listing-info"}>{listing.description}</div>
+      <div className={"listing-actions"}>
+        <p>
+          <span className={"bold"}>List Price:&nbsp;</span>
+          <span className={"listing-price"}>
+            ${listing.price.toLocaleString()}
+          </span>
+        </p>
+        <Button type={"button"} color={"primary"}>
+          Buy
+        </Button>
+        <Button type={"button"} color={"secondary"}>
+          Make Offer
+        </Button>
+      </div>
     </article>
   );
 };
