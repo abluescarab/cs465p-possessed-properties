b22771fd464774ccc241e5736111323cea4ca97a
Alana Gilston <abluescarab@users.noreply.github.com>
2023-05-17 06:02:51 -0700

    Removed testing colors from search page; updated sidebar

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,15 +1,51 @@
 import "./Sidebar.scss";
-import { ComponentBaseProps } from "@/components/ComponentBase.tsx";
-import { ListingType } from "../../../types.ts";
-
-interface SidebarProps extends ComponentBaseProps {
-  listings: ListingType[];
-}
+import { useEffect, useState } from "react";
+import { listingsLoader } from "@/Loaders.tsx";
+import { Link } from "react-router-dom";
 
 const Sidebar = ({ id = "", className = "" }) => {
+  const [regions, setRegions] = useState([]);
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getListings = async () => {
+      const allListings = await listingsLoader();
+
+      setRegions([...new Set(allListings.result.map((l) => l.region))]);
+      setCountries([...new Set(allListings.result.map((l) => l.country))]);
+    };
+
+    getListings();
+  }, []);
+
   return (
     <>
-      <aside className={"sidebar"}></aside>
+      <aside id={id} className={`sidebar ${className}`}>
+        <section className={"sidebar-section sidebar-region"}>
+          <h3>Region</h3>
+          {regions && (
+            <ul>
+              {regions.sort().map((region) => (
+                <li key={region}>
+                  <Link to={`/listings/region/${region}`}>{region}</Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </section>
+        <section className={"sidebar-section sidebar-country"}>
+          <h3>Country</h3>
+          {countries && (
+            <ul>
+              {countries.sort().map((country) => (
+                <li key={country}>
+                  <Link to={`/listings/region/${country}`}>{country}</Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </section>
+      </aside>
     </>
   );
 };
