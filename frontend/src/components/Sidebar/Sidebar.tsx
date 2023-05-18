962d251ca4cbd3b4ad7221284e55f903af67a499
Alana Gilston <abluescarab@users.noreply.github.com>
2023-05-18 03:21:43 -0700

    Added regions/countries to search sidebar

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -4,15 +4,24 @@ import { listingsLoader } from "@/Loaders.tsx";
 import { Link } from "react-router-dom";
 
 const Sidebar = ({ id = "", className = "" }) => {
-  const [regions, setRegions] = useState([]);
-  const [countries, setCountries] = useState([]);
+  const [regions, setRegions] = useState({});
+  const [countries, setCountries] = useState({});
 
-  // TODO: get all listings and map once?
   const getListings = async () => {
     const allListings = await listingsLoader();
+    const getRegions = {};
+    const getCountries = {};
 
-    setRegions([...new Set(allListings.result.map((l) => l.region))]);
-    setCountries([...new Set(allListings.result.map((l) => l.country))]);
+    allListings.result.forEach((listing) => {
+      const region = listing.region;
+      const country = listing.country;
+
+      getRegions[region] = (getRegions[region] || 0) + 1;
+      getCountries[country] = (getCountries[country] || 0) + 1;
+    });
+
+    setRegions(getRegions);
+    setCountries(getCountries);
   };
 
   useEffect(() => {
@@ -23,26 +32,32 @@ const Sidebar = ({ id = "", className = "" }) => {
     <>
       <aside id={id} className={`sidebar ${className}`}>
         <section className={"sidebar-section sidebar-region"}>
-          <h3>Region</h3>
+          <h3 className={"sidebar-header"}>Region</h3>
           {regions && (
-            <ul>
-              {regions.sort().map((region) => (
-                <li key={region}>
-                  <Link to={`/listings/region/${region}`}>{region}</Link>
-                </li>
-              ))}
+            <ul className={"sidebar-list"}>
+              {Object.keys(regions)
+                .sort()
+                .map((region) => (
+                  <li key={region}>
+                    <Link to={`/listings/region/${region}`}>{region}</Link> (
+                    {regions[region]})
+                  </li>
+                ))}
             </ul>
           )}
         </section>
         <section className={"sidebar-section sidebar-country"}>
-          <h3>Country</h3>
+          <h3 className={"sidebar-header"}>Country</h3>
           {countries && (
-            <ul>
-              {countries.sort().map((country) => (
-                <li key={country}>
-                  <Link to={`/listings/region/${country}`}>{country}</Link>
-                </li>
-              ))}
+            <ul className={"sidebar-list"}>
+              {Object.keys(countries)
+                .sort()
+                .map((country) => (
+                  <li key={country}>
+                    <Link to={`/listings/country/${country}`}>{country}</Link> (
+                    {countries[country]})
+                  </li>
+                ))}
             </ul>
           )}
         </section>
