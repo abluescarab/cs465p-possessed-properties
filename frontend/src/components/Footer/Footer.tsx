cc7a21a79c22d7c2a3f9bdca92345423b7cbcb28
Alana Gilston <abluescarab@users.noreply.github.com>
2023-05-21 21:27:25 -0700

    Changed footer links to sign up/sign in to profile

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -8,8 +8,7 @@ const Footer = () => {
       <Link to={Routes.search.path}>Buy</Link>
       <Link to={Routes.sell.path}>Sell</Link>
       <Link to={Routes.guide.path}>Guide</Link>
-      <Link to={Routes.signUp.path}>Sign up</Link>
-      <Link to={Routes.signIn.path}>Sign in</Link>
+      <Link to={Routes.profile.path}>Profile</Link>
       <div className={"full-width"}>&copy; Possessed Properties, Inc.</div>
     </footer>
   );
