3d3f0ae368d8aa71865f5d52412388b1e2dbb9a1
Alana Gilston <abluescarab@users.noreply.github.com>
2023-05-21 21:15:43 -0700

    Added footer links

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,7 +1,18 @@
 import "./Footer.scss";
+import { Routes } from "@/AppRouter.tsx";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer>
+      <Link to={Routes.search.path}>Buy</Link>
+      <Link to={Routes.sell.path}>Sell</Link>
+      <Link to={Routes.guide.path}>Guide</Link>
+      <Link to={Routes.signUp.path}>Sign up</Link>
+      <Link to={Routes.signIn.path}>Sign in</Link>
+      <div className={"full-width"}>&copy; Possessed Properties, Inc.</div>
+    </footer>
+  );
 };
 
 export default Footer;
