2ad73296986888ab20a91f1f1c76de424af8272c
Alana Gilston <abluescarab@users.noreply.github.com>
2023-05-23 03:11:19 -0700

    Added popup confirmation for buying listing

--- frontend/src/pages/Listing/Listing.tsx
+++ frontend/src/pages/Listing/Listing.tsx
@@ -1,6 +1,6 @@
 import "./Listing.scss";
 import { useLoaderData } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { setTitle } from "@/utils.tsx";
 import propertyImage from "@images/property.png";
 import Button from "@/components/Button/Button.tsx";
@@ -11,62 +11,96 @@ import Card, {
   CardSubtitle,
   CardTitle,
 } from "@/components/Card/Card.tsx";
+import Popup from "@/components/Popup/Popup.tsx";
+import axios from "axios";
 
 const Listing = () => {
   const loaderData: any = useLoaderData();
   const listing = loaderData.result;
+
+  const [showPopup, setShowPopup] = useState(false);
+
+  const showConfirmation = () => {
+    setShowPopup(true);
+  };
+
+  const confirmPurchase = async () => {
+    // TODO: send offer
+    await axios({
+      method: "POST",
+    });
+  };
+
+  const cancelPurchase = () => {
+    setShowPopup(false);
+  };
 
   useEffect(() => {
     setTitle(listing.name);
   }, [listing]);
 
   return (
-    <div id={"listing-page"} className={"page"}>
-      <SearchBar small />
-      <Crumbs
-        listing={listing.name}
-        region={listing.region}
-        country={listing.country}
-      />
-      <article className={"listing-article"}>
-        <div className={"listing-image"}>
-          <img src={propertyImage} alt={`Image of ${listing.name}`} />
-        </div>
-        <Card>
-          <CardTitle>{listing.name}</CardTitle>
-          <CardSubtitle>
-            {listing.address ? `${listing.address}, ` : ""}
-            {listing.region}, {listing.country}
-          </CardSubtitle>
-          <CardContent>
-            <p className={"listing-description"}>{listing.description}</p>
-            <div className={"listing-actions"}>
-              {/* TODO: add link to see offers on listing if owner */}
-              <p className={"font-lg"}>
-                <span className={"bold"}>List Price:&nbsp;</span>
-                <span className={"listing-price"}>
-                  ${listing.price.toLocaleString()}
-                </span>
-              </p>
-              <Button
-                type={"button"}
-                color={"primary"}
-                className={"action-button"}
-              >
-                Buy
-              </Button>
-              <Button
-                type={"button"}
-                color={"secondary"}
-                className={"action-button"}
-              >
-                Make Offer
-              </Button>
-            </div>
-          </CardContent>
-        </Card>
-      </article>
-    </div>
+    <>
+      <div id={"listing-page"} className={"page"}>
+        <SearchBar small />
+        <Crumbs
+          listing={listing.name}
+          region={listing.region}
+          country={listing.country}
+        />
+        <article className={"listing-article"}>
+          <div className={"listing-image"}>
+            <img src={propertyImage} alt={`Image of ${listing.name}`} />
+          </div>
+          <Card>
+            <CardTitle>{listing.name}</CardTitle>
+            <CardSubtitle>
+              {listing.address ? `${listing.address}, ` : ""}
+              {listing.region}, {listing.country}
+            </CardSubtitle>
+            <CardContent>
+              <p className={"listing-description"}>{listing.description}</p>
+              <div className={"listing-actions"}>
+                {/* TODO: add link to see offers on listing if owner */}
+                <p className={"font-lg"}>
+                  <span className={"bold"}>List Price:&nbsp;</span>
+                  <span className={"listing-price"}>
+                    ${listing.price.toLocaleString()}
+                  </span>
+                </p>
+                <Button
+                  type={"button"}
+                  color={"primary"}
+                  className={"action-button"}
+                  onClick={showConfirmation}
+                >
+                  Buy
+                </Button>
+                <Button
+                  type={"button"}
+                  color={"secondary"}
+                  className={"action-button"}
+                >
+                  Make Offer
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
+        </article>
+      </div>
+      {showPopup && (
+        <Popup
+          title={"Confirm Purchase"}
+          primaryButton={"Yes"}
+          secondaryButton={"No"}
+          primaryButtonOnClick={confirmPurchase}
+          secondaryButtonOnClick={cancelPurchase}
+        >
+          This action will send a purchase offer for the full price of the
+          listing. Are you sure you want to continue?
+        </Popup>
+      )}
+    </>
   );
 };
 
