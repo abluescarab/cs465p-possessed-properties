1de6ec11a3be4c3e9ec328602462b553f850623a
Alana Gilston <abluescarab@users.noreply.github.com>
2023-06-04 00:51:57 -0700

    Changed BackToTop to button

--- frontend/src/components/BackToTop/BackToTop.tsx
+++ frontend/src/components/BackToTop/BackToTop.tsx
@@ -1,10 +1,16 @@
 import "./BackToTop.scss";
+import Button from "@/components/Button/Button.tsx";
 
 const BackToTop = () => {
+  const scrollToTop = () => {
+    document.body.scrollTo(0, 0);
+    document.documentElement.scrollTo(0, 0);
+  };
+
   return (
-    <a id={"back-to-top"} href={"#"}>
+    <Button type={"button"} id={"back-to-top"} onClick={scrollToTop}>
       Back to top
-    </a>
+    </Button>
   );
 };
 
--- frontend/src/pages/Search/Search.tsx
+++ frontend/src/pages/Search/Search.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import { setTitle } from "@/utils.tsx";
 import ListingCard from "@/components/ListingCard/ListingCard.tsx";
 import Sidebar from "@/components/Sidebar/Sidebar.tsx";
+import BackToTop from "@/components/BackToTop/BackToTop.tsx";
 
 const Search = () => {
   const loaderData: any = useLoaderData();
@@ -18,26 +19,29 @@ const Search = () => {
 
   // TODO: add back to top link
   return (
-    <div id={"search-page"} className={"page"}>
-      <h2>{terms ? `Search for ${terms}` : "Browse all listings"}</h2>
-      <section className={"listing-search"}>
-        {listings.map((listing: any) => {
-          return (
-            <ListingCard
-              key={listing.id}
-              listingId={listing.id}
-              name={listing.name}
-              price={listing.price}
-              bedrooms={listing.bedrooms}
-              bathrooms={listing.bathrooms}
-              area={listing.area}
-              image={listing.imageUri}
-            />
-          );
-        })}
-      </section>
-      <Sidebar />
-    </div>
+    <>
+      <div id={"search-page"} className={"page"}>
+        <h2>{terms ? `Search for ${terms}` : "Browse all listings"}</h2>
+        <section className={"listing-search"}>
+          {listings.map((listing: any) => {
+            return (
+              <ListingCard
+                key={listing.id}
+                listingId={listing.id}
+                name={listing.name}
+                price={listing.price}
+                bedrooms={listing.bedrooms}
+                bathrooms={listing.bathrooms}
+                area={listing.area}
+                image={listing.imageUri}
+              />
+            );
+          })}
+        </section>
+        <Sidebar />
+      </div>
+      <BackToTop />
+    </>
   );
 };
 
